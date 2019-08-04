9fdb205321a68207c91337d4d02f285ef525c502
Si Kai Ng <sikai.ngsk@gmail.com>
2019-08-04 12:58:10 +0800

    Default + some of my additions

---
+++ keymap.c
@@ -0,0 +1,50 @@
+/*
+Base Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
+Modified 2017 Andrew Novak <ndrw.nvk@gmail.com>
+Modified 2018 Wayne Jones (WarmCatUK) <waynekjones@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public LicensezZZ
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#define ______ KC_TRNS
+#define _DEFLT 0
+#define _RAISE 1
+#define _LOWER 2
+#define _FN 3
+
+enum custom_keycodes {
+    P_MACRO = SAFE_RANGE
+};
+
+// GBP £ Macro (sends alt 156 - windows users only)
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch(keycode) {
+            case P_MACRO:
+                SEND_STRING(SS_DOWN(X_LALT));
+                SEND_STRING(SS_TAP(X_KP_1));
+                SEND_STRING(SS_TAP(X_KP_5));
+                SEND_STRING(SS_TAP(X_KP_6));
+                SEND_STRING(SS_UP(X_LALT));
+                return false; break;
+        }
+    }
+    return true;
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    /* Qwerty
