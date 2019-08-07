90b690669f4f363e5458a6dd0395c0d705530214
Si Kai Ng <sikai.ngsk@gmail.com>
2019-08-07 13:54:03 +0800

    More experiments with top and bottom row

--- keymap.c
+++ keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Qwerty
      * ,-----------------------------------------------------------------------------------.
-     * |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |  =   |
+     * |   1  |   2  |   3  |  GUI |   5  |   6  |   7  |   8  |  GUI |   0  |   -  |  =   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -54,15 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter | (Hold for shift / tap for enter)
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | _FN  | Ctrl | Alt  | Ctrl |Lower | GUI  |Space |Raise | GUI  | Down |  Up  |Right |
+     * | Shift| Ctrl | Alt  | Ctrl | GUI  |Lower |Raise |Space | GUI  | Down |  Up  |Shift |
      * `-----------------------------------------------------------------------------------'
      */
     [_DEFLT] = LAYOUT( \
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,       \
+        KC_1,    KC_2,    KC_3,    KC_LGUI, KC_5,    KC_6,    KC_7,    KC_8,    KC_RGUI, KC_0,    KC_MINS, KC_EQL,       \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,      \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT),\
-        MO(_FN), KC_LCTL, KC_LALT, KC_LCTL, MO(_LOWER),KC_LGUI,KC_SPC,MO(_RAISE),KC_LEFT, KC_RGUI, KC_UP,   KC_RGHT       \
+        KC_LSFT, KC_LCTL, KC_LALT, KC_LCTL, KC_LGUI, MO(_LOWER),MO(_RAISE),KC_SPC,KC_RGUI,KC_DOWN,  KC_UP, SFT_T(KC_ENT)  \
     ),
 
     /* Raise
