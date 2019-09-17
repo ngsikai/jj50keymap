c1e41d0a1c2299173811d39eda09ca12241ffd86
Si Kai <sikai.ngsk@gmail.com>
2019-09-17 22:44:59 +0800

    macro and shortcut fun

--- keymap.c
+++ keymap.c
@@ -24,7 +24,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _TRANS 4
 
 enum custom_keycodes {
-    P_MACRO = SAFE_RANGE
+    P_MACRO = SAFE_RANGE,
+    VS_CODE,
+    CHROME,
+    SLACK,
+    SPOTIFY,
 };
 
 // GBP £ Macro (sends alt 156 - windows users only)
@@ -38,6 +42,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_TAP(X_KP_6));
                 SEND_STRING(SS_UP(X_LALT));
                 return false; break;
+            case VS_CODE:
+                if (record->event.pressed) {
+                    SEND_STRING(SS_LGUI(" "));
+                    _delay_ms(70);
+                    SEND_STRING("code");
+                    _delay_ms(20);
+                    SEND_STRING(SS_TAP(X_ENTER));
+                    // _delay_ms(10);
+                }
+                return false;
+                break;
+            case CHROME:
+                if (record->event.pressed) {
+                    SEND_STRING(SS_LGUI(" "));
+                    _delay_ms(70);
+                    SEND_STRING("chrome");
+                    _delay_ms(20);
+                    SEND_STRING(SS_TAP(X_ENTER));
+                    // _delay_ms(10);
+                }
+                return false;
+                break;
+            case SLACK:
+                if (record->event.pressed) {
+                    SEND_STRING(SS_LGUI(" "));
+                    _delay_ms(70);
+                    SEND_STRING("slack");
+                    _delay_ms(20);
+                    SEND_STRING(SS_TAP(X_ENTER));
+                    // _delay_ms(10);
+                }
+                return false;
+                break;
+            case SPOTIFY:
+                if (record->event.pressed) {
+                    SEND_STRING(SS_LGUI(" "));
+                    _delay_ms(70);
+                    SEND_STRING("spotify");
+                    _delay_ms(20);
+                    SEND_STRING(SS_TAP(X_ENTER));
+                    // _delay_ms(10);
+                }
+                return false;
+                break;
         }
     }
     return true;
@@ -55,14 +103,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter | (Hold for shift / tap for enter)
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      | Ctrl |      | Alt  | GUI  | Nav  |Space | GUI  | Alt  | Caps | Ctrl |      |
+     * |  Fn  | Ctrl |      | Alt  | GUI  | Nav  |Space | GUI  | Alt  | Caps | Ctrl |      |
      * `-----------------------------------------------------------------------------------'
      */
     [_DEFLT] = LAYOUT( \
         LCTL(LGUI(KC_Q)), KC_1,    KC_2,    KC_3, KC_4, KC_5,    KC_6,    KC_7,    KC_8,    KC_9, KC_0, _______,       \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,      \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      \
-        SFT_T(KC_ENT), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),\
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),\
         MO(_FN), KC_LCTL, _______, KC_LALT, LM(_TRANS,MOD_LGUI), MO(_NAV), KC_SPC, LM(_TRANS,MOD_RGUI),KC_RALT,KC_CAPS, KC_RCTL,_______  \
     ),
 
@@ -91,9 +139,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      | Prev | Next |      |      |      |
+     * |      |      |      |SPOTIF|SLACK |      |      | Prev | Next | Alt<-| Alt->|      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      | Prev | Next |      | Left | Down |  Up  |Right |      |      |
+     * |      |VSCODE|CHROME| Prev | Next |      | Left | Down |  Up  |Right |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |      |      |      |      |      | Home | PDN  |  PUP |  End |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -103,8 +151,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV] = LAYOUT( \
        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-       _______, _______, _______, _______, _______, _______, _______, LGUI(KC_LCBR), LGUI(KC_RCBR), _______, _______, _______, \
-       _______, _______, _______, LGUI(KC_LCBR), LGUI(KC_RCBR), _______,  KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,  \
+       _______, _______, _______, SPOTIFY, SLACK  , _______, _______, LGUI(KC_LCBR), LGUI(KC_RCBR), LALT(KC_LEFT), LALT(KC_RIGHT), _______, \
+       _______, VS_CODE, CHROME , LGUI(KC_LCBR), LGUI(KC_RCBR), _______,  KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,  \
        _______, _______, _______, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,  \
        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
     ),
